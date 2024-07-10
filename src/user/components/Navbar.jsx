89b6b82ba8f44844c9f042940f8f76b47fcc2fba
IdinGanteng <kanggayus101@gmail.com>
2024-07-10 10:32:17 -0700

    create profile

---
+++ src/user/components/Navbar.jsx
@@ -0,0 +1,39 @@
+
+import React, { useState } from 'react';
+import '../user.css';
+import { FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="#" className="navbar-logo">MyWebsite</a>
+        <div className="menu-icon" onClick={toggleNavbar}>
+          <FaBars/>
+        </div>
+        <ul className={isOpen ? 'nav-menu active' : 'nav-menu'}>
+          <li className="nav-item">
+            <a href="#home" className="nav-links">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="#about" className="nav-links">About</a>
+          </li>
+          <li className="nav-item">
+            <a href="#services" className="nav-links">Services</a>
+          </li>
+          <li className="nav-item">
+            <a href="#contact" className="nav-links">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
