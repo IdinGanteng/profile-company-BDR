8c384832a24734fd3108d3316f02f7f3f8398390
IdinGanteng <kanggayus101@gmail.com>
2024-07-19 03:14:39 -0700

    hdfjdhf

--- src/user/components/WhatsappIcon.jsx
+++ src/user/components/WhatsappIcon.jsx
@@ -4,8 +4,13 @@ import waLogo from '../../assets/wa-logo2.png';
 const WhatsappIcon = () => {
   return (
     <div>
-        <a>
-          <img src={waLogo} className='whatsapp-icon'/>
+        <a
+           href="https://wa.me/6285848940211" 
+           className="whatsapp-icon" 
+           target="_blank" 
+           rel="noopener noreferrer"
+        >
+          <img src={waLogo} alt='logowa' className='whatsapp-icon'/>
         </a>
     </div>
   )
