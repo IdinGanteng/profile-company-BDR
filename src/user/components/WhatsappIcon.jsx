c213ef0a5fa53384280064d6a36d54c4fcfcadd3
IdinGanteng <kanggayus101@gmail.com>
2024-07-19 03:02:28 -0700

    whatsapp icon

--- src/user/components/WhatsappIcon.jsx
+++ src/user/components/WhatsappIcon.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { FaWhatsapp } from 'react-icons/fa';
+import waLogo from '../../assets/wa-logo2.png';
 
 const WhatsappIcon = () => {
   return (
     <div>
-       <a 
-        href="https://wa.me/6285848940211" 
-        className="whatsapp-icon" 
-        target="_blank" 
-        rel="noopener noreferrer"
-        >
-        <FaWhatsapp/>
+        <a>
+          <img src={waLogo} className='whatsapp-icon'/>
         </a>
     </div>
   )
